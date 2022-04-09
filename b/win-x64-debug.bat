b25a329d0678b5d4e5e824b177c13c93d93b698a
jimmy <mozeal@gmail.com>
2022-04-09 22:25:51 +0700

    Windows DemoApp

--- b/win-x64-debug.bat
+++ b/win-x64-debug.bat
@@ -1,10 +1,13 @@
-rm bin\gn
+del bin\gn
 bin\gn.exe gen out\win-x64-debug --args="target_cpu=\"x64\"" --ide=vs 
-echo ""
-echo "Build cmd:"
-echo "ninja -C out/win-x64-debug"
-echo ""
-echo "Run Test:"
-echo "out/win-x64-debug/HelloWorld"
-echo ""
-pause
+echo off
+echo -------------------------
+echo Build cmd:
+echo ninja -C out/win-x64-debug HelloWorldApp
+echo ninja -C out/win-x64-debug DemoApp
+echo -------------------------
+echo Run Test:
+echo out/win-x64-debug/HelloWorldApp
+echo out/win-x64-debug/DemoApp
+echo -------------------------
+
